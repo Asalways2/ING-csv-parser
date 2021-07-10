cc26e9cafc72952ce4ccabbf10197d76eef3fef3
ERBO Engineering <consultancy@erbo.io>
2021-07-10 23:51:36 +0200

    able to load a excel file and display the rows

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { utf8Encode } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ING-parser-excel';
+  public inputFileString: string = '';
+  public rows :string[] = [''];
+  
+  getINGfile(file : any){
+    this.fileToString(file);
+  }
+
+
+  fileToString(file:any){
+    const reader = new FileReader();
+    const tempfile = file.target.files[0];
+    reader.readAsText(tempfile);
+    reader.onload = (e) => {
+      const result = reader.result;
+      this.inputFileString = result as string;
+      this.rows = this.inputFileString.split('\n')
+    }
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MatSliderModule} from '@angular/material/slider';
+import { MaterialModule } from './material/material.module'
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +13,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
