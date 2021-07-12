eb3fa695b89ac21a04ea1c9dfb09b74d6718d432
ERBO Engineering <consultancy@erbo.io>
2021-07-12 16:02:37 +0200

    make minimal csv to 2D array parser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,74 @@
-import { utf8Encode } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ING-parser-excel';
-  public inputFileString: string = '';
-  public rows :string[] = [''];
-  
-  getINGfile(file : any){
-    this.fileToString(file);
+  public parsedfile: string[][] = [];
+  public amountOfEntrys: number = 0;
+  public rawFileString: string = '';
+
+  async getINGfile(file: any) {
+    this.rawFileString = await this.readFile(file);
+    console.log(this.CSVStringToArray(this.rawFileString));
   }
 
+  readFile(file: any): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      const tempfile = file.target.files[0];
+      reader.readAsText(tempfile);
+      reader.onload = (e) => {
+        resolve(reader.result as string);
+      };
+      reader.onerror = reject;
+    });
+  }
 
-  fileToString(file:any){
-    const reader = new FileReader();
-    const tempfile = file.target.files[0];
-    reader.readAsText(tempfile);
-    reader.onload = (e) => {
-      const result = reader.result;
-      this.inputFileString = result as string;
-      this.rows = this.inputFileString.split('\n')
+  // "inside of string"
+  // ; next entry
+  // space = next row
+
+  CSVStringToArray(string: String): string[][] {
+    let startOfString: Boolean = false;
+    let col = 0;
+    let row = 0;
+    let wordlen = 0;
+
+    let dataArray: string[][] = [];
+    let rowArray: string[] = [];
+
+    for (let len = 0; len <= string.length; len++) {
+      switch (string[len]) {
+        case '"': // start or end of string
+          startOfString = !startOfString; //toggle
+          if (startOfString === false) {
+            if (wordlen > 0) {
+              rowArray.push(string.substring(len + 1 - wordlen, len));
+            }
+            wordlen = 0;
+          }
+          break;
+        case ';': // next col
+          col++;
+          break;
+        case '\n': // if not in string then new row
+          if (startOfString) {
+            break;
+          }
+          row++;
+          dataArray.push(rowArray);
+          rowArray = [];
+          break;
+      }
+
+      if (startOfString) {
+        wordlen++;
+      }
     }
+    return dataArray;
   }
-
-
-
 }
